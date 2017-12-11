0c52217feb3b39d39cd6fc6d8ee8e40011817e5c
D Bruce Saurer <notami@gmail.com>
2017-12-11 10:49:29 -0500

    new version of tridactyl

--- personal/611-install-firefox-extensions-v1.sh
+++ personal/611-install-firefox-extensions-v1.sh
@@ -20,7 +20,7 @@ mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/do
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/782402/privacy_badger-2017.11.20-an+fx.xpi
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/803415/https_everywhere-2017.12.6-an+fx.xpi
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/801244/self_destroying_cookies-0.104-an+fx.xpi
-mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/797772/tridactyl-1.7.0-an+fx.xpi
+mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/807877/tridactyl-1.7.0pre562-an+fx.xpi
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/766725/nosquint_plus-62.1-an+fx.xpi
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/723895/dark_theme_for_youtubetm-0.1.8-an+fx.xpi
 mozilla-extension-manager --install --user https://addons.mozilla.org/firefox/downloads/file/791438/bloody_vikings-0.9.3-an+fx-linux.xpi
